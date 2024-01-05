183d3f08bd3464db026d73005278bec84f8a8fe7
Mudi U <mudi@renegare.com>
2024-01-05 17:12:25 +0000

    Add logged in page

---
+++ client/src/app/loggedin/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Flex, Heading } from '@chakra-ui/react';
+
+export default function LoggedIn() {
+
+  
+  return (
+    <Flex>
+      <Heading>Logged In</Heading>
+    </Flex>
+  );
+}
--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,14 +1,29 @@
-import { Button, Flex, Heading, Input } from "@chakra-ui/react";
+'use client';
+
+import { Button, Flex, Heading, Input } from '@chakra-ui/react';
+import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+
+  const onSubmit = useCallback((data: any) => {
+
+    router.replace('/loggedin');
+  }, []);
+  
   return (
     <Flex>
       <Heading>Login</Heading>
 
-      <Input name="email" />
-      <Input type="password" name="password" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input {...register('email')} />
+        <Input type="password" {...register('password')} />
 
-      <Button type="submit">Login</Button>
+        <Button type="submit">Login</Button>
+      </form>
     </Flex>
-  )
+  );
 }
