e796c1e84df6ff5b9ce576998c53d9025891436c
Mudi U <mudi@renegare.com>
2024-01-05 12:38:29 +0000

    add test to login

--- test/cypress/e2e/login.cy.js
+++ test/cypress/e2e/login.cy.js
@@ -1,5 +1,10 @@
+const clientURL = 'http://localhost:3000/login';
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit(clientURL);
+    cy.get('email').type('username');
+    cy.get('password').type('password');
+    cy.get('button').click();
   })
 })
